197d988054ae98b7c8067f1d354405dfddf8817e
Oisin Cleary <clearyoi@tcd.ie>
2018-12-02 11:12:38 +0000

    part 1

---
+++ 2/part1.py
@@ -0,0 +1,32 @@
+class DuplicatesCounter(object):
+
+    def __init__(self):
+        self.doubles = 0
+        self.triples = 0
+
+    def count_letters(self, word):
+        letter_counts = {}
+        for letter in word:
+            if letter in letter_counts:
+                letter_counts[letter] += 1
+            else:
+                letter_counts[letter] = 1
+        return letter_counts
+
+    def count_duplicates(self, word):
+        letter_counts = self.count_letters(word)
+        if 2 in letter_counts.values():
+            self.doubles += 1
+        if 3 in letter_counts.values():
+            self.triples += 1
+
+    def perform_work(self, words):
+        for word in words:
+            self.count_duplicates(word)
+        print self.doubles
+        print self.triples
+        return self.doubles * self.triples
+
+words = [x for x in open("input.txt").read().strip().split('\n')]
+counter = DuplicatesCounter()
+print counter.perform_work(words)
