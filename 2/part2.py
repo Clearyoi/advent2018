c4943a8c099ecbe31a5c6df56270725cfd357634
Oisin Cleary <clearyoi@tcd.ie>
2018-12-02 11:42:51 +0000

    part 2

---
+++ 2/part2.py
@@ -0,0 +1,25 @@
+class NearDuplicateFinder(object):
+
+    def __init__(self):
+        self.match = ""
+
+    def check_for_duplicates(self, short_words):
+        list_of_words = set()
+        for word in short_words:
+            if word in list_of_words:
+                self.match = word
+                break
+            else:
+                list_of_words.add(word)
+
+    def perform_work(self, words):
+        for i in range(len(words[0])):
+            short_words = [(x[:i]+x[i+1:]) for x in words]
+            self.check_for_duplicates(short_words)
+            if self.match != "":
+                break
+        return self.match
+
+words = [x for x in open("input.txt").read().strip().split('\n')]
+finder = NearDuplicateFinder()
+print finder.perform_work(words)
